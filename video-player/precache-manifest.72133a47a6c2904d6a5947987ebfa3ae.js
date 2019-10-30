90b35e00c7e882fc5a907ef898ea7f7f25513bc5
Ayaskant Mishra <ayas.optimark@gmail.com>
2019-10-30 16:17:42 +0530

    Added video-react/hls  videoplayer to backdrop

---
+++ video-player/precache-manifest.72133a47a6c2904d6a5947987ebfa3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed33327d76758003518ec4e798e93998",
+    "url": "/video-player/index.html"
+  },
+  {
+    "revision": "bcede68adda850f7579c",
+    "url": "/video-player/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "98faa6e1a0f90dff1e59",
+    "url": "/video-player/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "bcede68adda850f7579c",
+    "url": "/video-player/static/js/2.5e434e85.chunk.js"
+  },
+  {
+    "revision": "98faa6e1a0f90dff1e59",
+    "url": "/video-player/static/js/main.7fc93f4c.chunk.js"
+  },
+  {
+    "revision": "f7d7a014617a7f8ecc6d",
+    "url": "/video-player/static/js/runtime-main.402cee81.js"
+  }
+]);
